5d8a4b5456a58b5b94bcd323a9fec919fca0994b
MikePio <michelepiopilla@gmail.com>
2023-05-18 17:48:21 +0200

    Created php file and readme.md

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+$test = '$test';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- BOOTSTRAP -->
+  <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css">   -->
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css' integrity='sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==' crossorigin='anonymous' />
+  <title>PHP HOTEL</title>
+</head>
+
+<body class="bg-dark text-white">
+  <h1>test</h1>
+  <h2><?php echo $test ?></h2>
+  
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script> -->
+
+
+</body>
+
+</html>
